8274b1d2b7b71ca5011097ba78297d85d5307e23
dsw55357 <dsw55357@student.dsw.edu.pl>
2024-04-20 13:41:13 +0200

    logs function added

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 
+function logs
+{
+    echo $@
+    # wywołanie: skrypt.sh --logs
+    if [ $# -lt 2 ]; then
+	for i in $(seq 1 100); do
+	    nazwa_pliku="log$i.txt"
+	    touch $nazwa_pliku
+	    echo $nazwa_pliku > $nazwa_pliku
+	    echo $0 >> $nazwa_pliku
+	    echo $(date) >> $nazwa_pliku
+	done
+    fi
+}
 
-function menu2
+
+function menu
 {
     #echo $1
     case $1 in
 	--date) date ;;
+	--logs | -l) logs $@ ;;
 	*) help
     esac
 }
@@ -14,7 +30,7 @@ function menu2
 
 function main
 {
-    menu2 $@
+    menu $@
 }
 
 main $@
